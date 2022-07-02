f359e860519fd01db2df3778f1aa7f2893e76b13
vamshinirumalla <108589412+vamshinirumalla@users.noreply.github.com>
2022-07-03 00:04:31 +0530

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,12 @@
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = {
+    randomIntFromInterval,
+    sleep
+}
